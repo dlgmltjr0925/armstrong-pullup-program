9ed1620e2ff0a1d5b9827fbbbae97f65eaed2550
fine <dlgmltjr0925@gmail.com>
2021-03-24 17:44:52 +0900

    Add profile

--- pages/api/profile/index.ts
+++ pages/api/profile/index.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-
-import { getAllProfiles } from '../../../utils/profile';
+import { getAllProfiles, insertProfile } from '../../../utils/profile';
 
 const handler = (_req: NextApiRequest, res: NextApiResponse) => {
   try {
@@ -8,6 +7,12 @@ const handler = (_req: NextApiRequest, res: NextApiResponse) => {
       const profiles = getAllProfiles();
       res.status(200).json({
         profiles,
+      });
+    } else if (_req.method === 'POST') {
+      const { nickname } = _req.body;
+      const profile = insertProfile(nickname);
+      res.status(200).json({
+        profile,
       });
     }
   } catch (err) {
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,34 @@
 import { useCallback, useEffect, useState } from 'react';
 
-import { GetServerSideProps } from 'next';
+import Layout from '../components/Layout';
 import { Member } from '../interfaces';
 import axios from 'axios';
-import { getAllProfiles } from '../utils/profile';
 
 interface LoginPageProps {
   profiles?: Member[];
 }
 
 const LoginPage = ({ ...props }: LoginPageProps) => {
-  const [profiles, setProfiles] = useState<Member[]>(props.profiles || []);
+  const [profiles, setProfiles] = useState<Member[]>([]);
+
+  const [nickname, setNickname] = useState<string>('');
+
+  const handleChangeNickname = (e: any) => {
+    setNickname(e.target.value);
+  };
+
+  const handleClickAdd = useCallback(async () => {
+    try {
+      const res = await axios.post('/api/profile', { nickname });
+      if (res && res.data) {
+        const { profile } = res.data;
+        setProfiles([...profiles, profile]);
+        setNickname('');
+      }
+    } catch (error) {
+      throw error;
+    }
+  }, [nickname]);
 
   const getProfiles = useCallback(async () => {
     try {
@@ -28,27 +46,21 @@ const LoginPage = ({ ...props }: LoginPageProps) => {
     if (!props.profiles) getProfiles();
   }, []);
 
-  console.log(profiles);
-
   return (
-    <div>
-      {profiles.map((profile) => {
-        return <div key={`${profile.id}`}>{profile.nickname}</div>;
-      })}
-    </div>
+    <Layout title='Login'>
+      <div>
+        {profiles.map((profile) => {
+          return <div key={`${profile.id}`}>{profile.nickname}</div>;
+        })}
+      </div>
+      {profiles.length < 4 && (
+        <>
+          <input type='text' value={nickname} onChange={handleChangeNickname} />
+          <button onClick={handleClickAdd}>+</button>
+        </>
+      )}
+    </Layout>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const profiles = getAllProfiles();
-
-  console.log(profiles);
-
-  return {
-    props: {
-      profiles,
-    },
-  };
 };
 
 export default LoginPage;
