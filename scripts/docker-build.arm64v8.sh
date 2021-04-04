ac6ba86de4ab13ae1a4c4dc9f17449a3fa015f73
fine <dlgmltjr0925@gmail.com>
2021-04-04 22:35:30 +0900

    Fix docker image tag

--- scripts/docker-build.arm64v8.sh
+++ scripts/docker-build.arm64v8.sh
@@ -3,4 +3,4 @@
 rm -rf node_modules
 yarn && yarn build
 tar -cf armstrong.tar .next .gitignore next.config.js package.json tsconfig.json yarn.lock
-docker build . -f dockers/Dockerfile.arm64v8 -t dlgmltjr0925/armstrong-pullup-arm64v8:latest
+docker build . -f dockers/Dockerfile.arm64v8 -t dlgmltjr0925/armstrong-pullup-program:arm64v8-latest
--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -3,4 +3,4 @@
 rm -rf node_modules
 yarn && yarn build
 tar -cf armstrong.tar .next .gitignore next.config.js package.json tsconfig.json yarn.lock
-docker build . -f dockers/Dockerfile -t dlgmltjr0925/armstrong-pullup:latest
+docker build . -f dockers/Dockerfile -t dlgmltjr0925/armstrong-pullup-program:latest
